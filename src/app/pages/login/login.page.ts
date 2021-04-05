5e3f8eb3667b6b88f91ec97723adcea52a9a4775
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-04-05 07:08:59 +0100

    Added google auth

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import "@codetrix-studio/capacitor-google-auth";
+import { Plugins } from '@capacitor/core';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  userInfo = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async googleSignup() {
+    const googleUser = await Plugins.GoogleAuth.signIn();
+    console.log('user: ', googleUser);
+    this.userInfo = googleUser;
   }
 
 }
